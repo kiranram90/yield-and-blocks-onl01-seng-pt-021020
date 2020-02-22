7b3eb848e4bfab71671826e2600e6ba6d86e349e
kiran ram <kram@lions.molloy.edu>
2020-02-22 05:57:32 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,7 +4,16 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   end
 end
 
-
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
+end
 
 # call your method here!
 
